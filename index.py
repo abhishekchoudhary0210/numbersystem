2cbb7058669fdbc87fcc0449fbc687ef7b09a174
abhishekchoudhary0210 <abhishekfbd0210@gmail.com>
2024-12-24 20:33:50 +0530

    index.py file has been added

---
+++ index.py
@@ -0,0 +1,10 @@
+numbers = [ 3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18]
+even=[]
+odd=[]
+for i in numbers:
+    if(i%2==0):
+      even.append(i)  
+    else:
+       odd.append(i)
+print(f"Even Numbers are:{even}")
+print(f"Even Numbers are:{odd}")
